2817f13b8352406bc95006982c6386f7fa4b6cf8
Georg Jaehnig <georg@jaehnig.org>
2020-03-17 04:17:04 +0100

    Working with Bootstrap.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function out(str) {
 }
 
 const fs = require('fs');
-const input = fs.readFileSync('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv2').toString()
+const input = fs.readFileSync('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv').toString()
 
 const locations = parse(input, {
   skip_empty_lines: true
@@ -22,41 +22,35 @@ for (location of locations.slice(1)) {
   const row = {};
 
   row[columns[1]] = location[1]
+  row[columns[0]] = location[0]
 
-  out('<tr valign="top">')
-  out('<td>')
-  out(location[1])
-  out('</td>')
-  out('<td>')
-  out(location[0])
-  out('</td>')
   let previous_value;
   let sum_growths = 0;
+  let day = -6;
   for (current_value of location.slice(-7)) {
-    out('<td>')
-    out(current_value)
-    out('</td>')
-    out('<td>')
+    row['day' + day + 'value'] = current_value
     if (previous_value > 0) {
       let current_growth = (current_value/previous_value - 1)
       if (current_growth != 0) {
+        let current_growth_str = ''
         if (current_growth > 0) {
-          out('+')
+          current_growth_str += '+'
         }
-        out(current_growth.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0}))
+        current_growth_str += current_growth.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0})
+        current_growth_str = (current_growth*100).toLocaleString(undefined,{maximumFractionDigits:0})
+        row['day' + day + 'growth'] = current_growth_str
       }
       sum_growths += current_growth
     }
     previous_value = current_value
-    out('</td>')
+    day++
   }
-  out('<td valign="bottom">')
-  const avg_growth = sum_growths / 6;
-  out(avg_growth.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0}))
-  out('</td>')
-  out('</tr>')
+
+  const average_growth = sum_growths / 6;
+  //row['average_growth'] = average_growth.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0})
+  row['average_growth'] = (average_growth*100).toLocaleString(undefined,{maximumFractionDigits:0})
   rows.push(row)
 }
 out('</table>')
 
-console.log(rows)
+console.log(JSON.stringify(rows))

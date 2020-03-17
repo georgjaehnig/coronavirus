12b5b64cfcb24b3aae15a8e460fcf86f26426c00
Georg Jaehnig <georg@jaehnig.org>
2020-03-17 17:10:06 +0100

    Add dates to columns.

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+fetch('json/columns.json')
+  .then(response => response.json())
+  .then(json => {
+
+    for (i=-4; i<=0; i++) {
+      let date = json[json.length + i -1]
+      date = date.replace(/\/20$/, '')
+      document.querySelector('th[data-field="day' + i + 'value"] .th-inner').innerHTML = date;
+    }
+
+
+  });
+

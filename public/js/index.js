2b49adc6a2f5e20578b55420b4d3ab3c01a9ea7f
Georg Jaehnig <georg@jaehnig.org>
2020-03-22 00:42:14 +0100

    Remove year from date.

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,8 @@ fetch('json/columns.json')
 
     for (i=-4; i<=0; i++) {
       let date = json[json.length + i -1]
+
+      // Remove year from date.
       date = date.replace(/\/20$/, '')
       document.querySelector('th[data-field="day' + i + 'value"] .th-inner').innerHTML = date;
     }

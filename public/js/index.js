15a6d26daaec6dc8e53d663f723956c967457aaa
Georg Jaehnig <georg@jaehnig.org>
2020-03-22 01:02:34 +0100

    Add toggle source.

--- public/js/index.js
+++ public/js/index.js
@@ -14,3 +14,14 @@ fetch('json/columns.json')
 
   });
 
+function setTab(source) {
+
+  // Set new source.
+  $('#dataTable').bootstrapTable('refresh', {
+    url: 'json/' + source + '.json'
+  });
+
+  // Make new tab active.
+  $(".nav .nav-link").removeClass("active");
+  $(".nav .nav-item." + source + " .nav-link").addClass("active");
+}

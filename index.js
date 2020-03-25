ef0e2801e83e3a3c4d9c356ffb486076bd3bc5e6
Georg Jaehnig <georg@jaehnig.org>
2020-03-25 18:15:31 +0100

    Output int.

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ function writeData(csvFileName, jsonFileName) {
     let product_growths = 1;
     let day = -4;
     for (current_value of location.slice(-5)) {
-      row['day' + day + 'value'] = current_value.toLocaleString(undefined,{style: 'decimal'})
+      row['day' + day + 'value'] = parseInt(current_value)
       if ((previous_value > 0) && (current_value > 0)) {
         let current_growth = (current_value/previous_value)
-        const current_growth_str = ((current_growth-1)*100).toLocaleString(undefined,{maximumFractionDigits:0})
+        const current_growth_str = Math.round(((current_growth-1))*100)
         row['day' + day + 'growth'] = current_growth_str
         product_growths = product_growths * current_growth
       }
@@ -42,7 +42,7 @@ function writeData(csvFileName, jsonFileName) {
     }
   
     const average_growth = Math.pow(product_growths, 1/4)
-    row['average_growth'] = ((average_growth-1)*100).toLocaleString(undefined,{maximumFractionDigits:0})
+    row['average_growth'] = Math.round((average_growth-1)*100)
     rows.push(row)
   }
   fs.writeFileSync(jsonFileName, JSON.stringify(rows))

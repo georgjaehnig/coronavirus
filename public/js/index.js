ec5b7e6623eb1a08a3784626293b0b7ddb0f50fd
Georg Jaehnig <georg@jaehnig.org>
2020-03-25 16:33:44 +0100

    Into function.

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,20 @@
-// Set column headers.
-fetch('json/columns.json')
-  .then(response => response.json())
-  .then(json => {
 
-    for (i=-4; i<=0; i++) {
-      let date = json[json.length + i -1]
+function setColumnHeaders() {
+  fetch('json/columns.json')
+    .then(response => response.json())
+    .then(json => {
+  
+      for (i=-4; i<=0; i++) {
+        let date = json[json.length + i -1]
+  
+        // Remove year from date.
+        date = date.replace(/\/20$/, '')
+        document.querySelector('th[data-field="day' + i + 'value"] .th-inner').innerHTML = date;
+      }
+    });
+}
 
-      // Remove year from date.
-      date = date.replace(/\/20$/, '')
-      document.querySelector('th[data-field="day' + i + 'value"] .th-inner').innerHTML = date;
-    }
-
-
-  });
+setColumnHeaders();
 
 function setTab(source) {
 
